c54e23b3a5764fa09c547b4b4b23ced4f15cd8b3
Vineet Pande <vsp4991@gmail.com>
2021-02-12 07:06:55 +0000

    done

--- fetcher.js
+++ fetcher.js
@@ -1,28 +1,53 @@
 const request = require('request');
 const fs = require('fs');
 
+const readline = require('readline'); 
+// const w = () => {
+
+  
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 const d = process.argv;
 let w = d.slice(2);
 
-request(w[0], (error, response, body) => {
+const write = (w, content) => {
+  fs.writeFile(`./${w}`, content, (err) => {  
+    if (err) {
+      return console.log('Error writing file')
+    } else {
+      return console.log(`${content.length} bytes written to ${w}`)
+    }
+  });
+}
+
+
+const q = request(w[0], (error, response, body) => {
   console.log('error:', error); 
   console.log('statusCode:', response && response.statusCode ); 
   console.log('body:', body.length); 
-  if (error !== null) {
-    
+  if (error !== null) {   
     console.log("The resource not found");
   } else {
-    fs.writeFile(`./${w[1]}`, body, (err) => {
-        if (err) {
-          console.log('Error writing file')
-        } else {
-          console.log(`${body.length} bytes written to ${w[1]}`)
-        }
+    fs.readFile(`./${w[1]}`, 'utf8', (erro) => {
+      if (erro) {  
+        write(w[1], body);
+        process.exit(); 
+      } else {
+        console.log("sorry it exists");
+        rl.question('provide input: ', (a) => {
+          if (a === 'Y') {
+            write(w[1], body);   
+          } 
+          rl.close();
+          process.exit();
+        });
+      }
     });
-  }
-  // if ()
-  // fs.writeFile(`./${w[1]}`, body, (err) => {
-
-  // })
-    
+  }   
 });
+
+q;
